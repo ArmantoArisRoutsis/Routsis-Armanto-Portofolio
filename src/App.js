eff66d7de85f70c27e43a42c822b09f362750517
Routsis Armanto <ar.routsis@gmail.com>
2021-03-19 19:34:46 +0200

    Minor Update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './components/Layout/Navbar';
 import Footer from './components/Layout/Footer';
 import HomePage from './components/Pages/HomePage';
-import CerificationsPage from "./components/Pages/CerificationsPage";
+import CertificationsPage from "./components/Pages/CertificationsPage";
 
 import "aos/dist/aos.css";
 
@@ -15,7 +15,7 @@ const App = () => {
       <Navbar/>
         <Switch>
           <Route path="/" exact component={HomePage}/>
-          <Route path="/certifications" exact component={CerificationsPage}/>
+          <Route path="/certifications" exact component={CertificationsPage}/>
         </Switch>
       <Footer/>
     </Router>
